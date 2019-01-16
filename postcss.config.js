ddb7c28f8156a308c11671a8286a4fb9ac35ffd6
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2019-01-16 17:18:22 -0600

    Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-simple-vars'),
-    require('postcss-media-minmax'),
     require('postcss-nested'),
     require('postcss-strip-inline-comments'),
     require('postcss-preset-env')
