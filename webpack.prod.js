bfa356169abfe565c647fd11c95421aed37da039
Gunnar Johnson <gunnarmjohnson@gmail.com>
2019-07-08 16:22:01 -0500

    Update dependencies, modify Webpack config (production)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
@@ -30,6 +30,6 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({filename: 'styles.css'}),
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin()
   ]
 });
