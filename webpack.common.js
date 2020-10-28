4e6e72c1af08d783a9dd2623ce5e8f7358b28557
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2020-10-28 15:45:18 -0500

    Fix favicon path bug

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path');
-let FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     // new FaviconsWebpackPlugin({
     //   logo: './src/assets/icons/icon.png',
-    //   prefix: 'assets/icons/',
+    //   prefix: './assets/icons/',
     //   title: '',
     //   icons: {
     //     android: true,
