0af77534bbe67ab21e1aa2095c532b94d2eb923e
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2024-03-25 09:51:20 -0500

    Add ESLint and Prettier - Update package.json   - Bump version to 2.0.0   - Restructure file   - Add ESLint dependencies   - Add Prettier dependencies   - Add postcss-media-minmax and prop-types dependencies   - Update favicons-webpack-plugin dependency   - Add scripts   - Specify node version in engines field - Update lockfile - Add editor config file - Add ESLint config files - Add Prettier config file - Add favicon-react image - Update Webpack config file to use Favicons Webpack Plugin - Update README - Format code - Resolve linting errors/warnings

--- src/components/ReactComponent.js
+++ src/components/ReactComponent.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact } from '@fortawesome/free-brands-svg-icons/faReact';
 
-const ReactComponent = (props) => (
+const ReactComponent = ({ subtitle, title }) => (
   <div className="react-component">
-    <FontAwesomeIcon className="react-component__icon" icon={faReact} />
-    <h1 className="react-component__title">{props.title}</h1>
-    <p className="react-component__content">{props.subtitle}</p>
+    <FontAwesomeIcon
+      className="react-component__icon"
+      icon={faReact}
+    />
+    <h1 className="react-component__title">{title}</h1>
+    <p className="react-component__content">{subtitle}</p>
   </div>
 );
 
 ReactComponent.defaultProps = {
-  title: 'React Boilerplate',
   subtitle: 'Start building',
+  title: 'React Boilerplate',
+};
+
+ReactComponent.propTypes = {
+  subtitle: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default ReactComponent;
