4c4ed6577b8a8e829deb93986dd76282c2f6a4fe
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-04-24 11:01:28 -0500

    Migrate development server - Update package.json   - Bump version to 3.1.0   - Update dev script   - Add webpack-dev-server dependency   - Remove webpack-plugin-serve dependency - Update lockfile - Update Webpack dev config   - Add devServer configuration   - Remove webpack-plugin-serve configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  entry: ['webpack-plugin-serve/client'],
   mode: 'development',
   module: {
     rules: [
@@ -24,14 +22,13 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [
-    new Serve({
-      host: 'localhost',
-      liveReload: true,
-      open: true,
-      port: 8080,
-      static: [path.resolve(__dirname, 'dist')],
-    }),
-  ],
-  watch: true,
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+  },
 });
